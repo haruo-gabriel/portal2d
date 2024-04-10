207d46fa1931d8af2a6bb7f6cbd78ee49597b7f4
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-04-09 23:52:04 -0300

    Adding a `jump` function.
    The jump animation is called there.

--- godot project/Scripts/Player/Player.gd
+++ godot project/Scripts/Player/Player.gd
@@ -2,15 +2,16 @@
 extends CharacterBody2D
 
 const SPEED: float = 300.0
-const JUMP_VELOCITY: float = -600.0
+const JUMP_VELOCITY: float = -600.0 / 1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") / 8
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 
 var direction: float
+var jumped: bool
 
 func set_animation() -> void:
 	"""
@@ -18,11 +19,16 @@ func set_animation() -> void:
 	direction and vertical speed.
 	"""
 	
+	# animation.play("Jump") is called in `jump`
+	# adding it here with a `velocity < 0` check would make the 
+	# animation restart while the player is in the air
+	
 	if velocity.y > 0:
 		return animation.play("Fall") # Returns early so others don't affect it
-	elif velocity.y < 0:
-		return animation.play("Jump")
-
+	
+	if not is_on_floor():
+		return
+	
 	# This part should only be reached
 	# if the player is on the ground.
 	
@@ -30,20 +36,29 @@ func set_animation() -> void:
 		animation.play("Idle")
 	else:
 		animation.play("Walk")
+
+
+func jump() -> void:
+
+	velocity.y = JUMP_VELOCITY
 	
-	
+	animation.play("Jump")
+
+
 func _physics_process(delta: float) -> void:
 
 	direction = Input.get_axis("move_left", "move_right")
+	jumped = Input.is_action_just_pressed("jump") and is_on_floor()
 
 	set_animation()
+	
+	if jumped:
+		jump()
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	
 	if direction < 0:
 		sprite.flip_h = 1
 	else:
