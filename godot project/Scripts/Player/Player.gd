d126b7358f6dd58030defa59ad4fc9dfa71bec6a
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-04-10 00:13:16 -0300

    Adding a function for flipping the sprite.
    Before, it defaulted to right if the player wasn't moving. Now it doesn't change the direction.
    
    Also changed the animation so "Idle" only plays when the player is grounded and not moving.
    
     Before it would be Idle if the player was grounded and there was no horizontal input.

--- godot project/Scripts/Player/Player.gd
+++ godot project/Scripts/Player/Player.gd
@@ -5,6 +5,7 @@ const SPEED: float = 300.0
 const JUMP_VELOCITY: float = -600.0
 
 const MAX_Y_SPEED: float = 1000 # In absolute value
+const MAX_X_SPEED: float = 400 # In absolute value
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
@@ -33,10 +34,18 @@ func set_animation() -> void:
 	# This part should only be reached
 	# if the player is on the ground.
 	
-	if not direction:
+	if not velocity.x:
 		animation.play("Idle")
 	else:
 		animation.play("Walk")
+
+func set_sprite() -> void:
+	
+	# We don't flip if we aren't moving
+	if not direction: 
+		return
+	
+	sprite.flip_h = direction < 0
 
 func jump() -> void:
 
@@ -62,18 +71,17 @@ func move_horizontal(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED / 60)
 
+	# Ensures velocity.x is within bounds
+	velocity.x = max(-MAX_X_SPEED, min(MAX_X_SPEED, velocity.x))
+
 func _physics_process(delta: float) -> void:
 
 	direction = Input.get_axis("move_left", "move_right")
 	jumped = Input.is_action_just_pressed("jump") and is_on_floor()
 
 	set_animation()
+	set_sprite()
 	move_vertical(delta)
 	move_horizontal(delta)
-
-	if direction < 0:
-		sprite.flip_h = 1
-	else:
-		sprite.flip_h = 0
 
 	move_and_slide()
