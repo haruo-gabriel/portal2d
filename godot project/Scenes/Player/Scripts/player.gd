f1406e6f32918506b03997a73ddd93514a7b56a7
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-04-17 13:26:24 -0300

    Removing redundancies 2
    I forgot to save this last bit before the last commit

--- godot project/Scenes/Player/Scripts/player.gd
+++ godot project/Scenes/Player/Scripts/player.gd
@@ -8,11 +8,6 @@ extends CharacterBody2D
 @onready var main_hitbox: CollisionShape2D = $MainHitbox
 @onready var crouched_hitbox: CollisionShape2D = $CrouchedHitbox
 
-func set_stats() -> void:
-
-	position = stats.position
-	velocity = stats.velocity
-
 func set_angle() -> void:
 	
 	var difference: Vector2 = get_global_mouse_position() - (global_position + constants.PLAYER_CENTER)
@@ -35,8 +30,7 @@ func toggle_crouch(to_crouch: bool) -> void:
 	if move_and_collide(Vector2.ZERO, true):
 		toggle_hitbox(not to_crouch)
 
-
-func move_vertical(delta: float) -> void:
+func try_jump() -> void:
 	
 	if Input.is_action_just_pressed("jump"):
 		stats.last_jumped = constants.JUMP_BUFFER_TIME
@@ -49,40 +43,17 @@ func move_vertical(delta: float) -> void:
 
 	stats.last_jumped = max(0, stats.last_jumped - 1)
 
-func move_horizontal(_delta: float) -> void:
-
-	# TODO: make this not so ulgy
-
-	if stats.direction:
-		
-		var step = constants.HORIZONTAL_ACCELERATION
-		var new_speed = stats.direction * constants.WALKING_SPEED
-		
-		# You won't be slowed while on air
-		if stats.is_crouching and is_on_floor():
-			new_speed *= constants.CROUCH_SPEED_MULTIPLIER
-			
-		if not is_on_floor():
-			
-			step *= constants.IN_AIR_MULTIPLIER
-			
-			# If you're going foward, you won't deaccelerate.
-			# If you wanna go backward, you still can.
-			if sign(velocity.x) == sign(stats.direction):
-				new_speed = sign(new_speed) * max(abs(new_speed), abs(velocity.x))
-		
-		velocity.x = move_toward(velocity.x, new_speed, step)
-
-	elif is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, constants.GROUND_DRAG)
-
-func move(delta: float) -> void:
+func move() -> void:
 	
-	move_horizontal(delta)
-	move_vertical(delta)
+	try_jump()
 
 	velocity.x = clamp(velocity.x, -constants.MAX_X_SPEED, constants.MAX_X_SPEED)
 	velocity.y = clamp(velocity.y, -constants.MAX_Y_SPEED, constants.MAX_Y_SPEED)
+
+	move_and_slide()
+
+	stats.position = position
+	stats.velocity = velocity
 
 func _ready() -> void:
 	
@@ -102,13 +73,5 @@ func _physics_process(delta: float) -> void:
 	if not try_crouch and stats.is_crouching:
 		toggle_crouch(false)
 
-	move_vertical(delta)
-	# move(delta)
-
 	set_angle()
-	set_stats()
-
-	move_and_slide()
-
-	stats.position = position
-	stats.velocity = velocity
+	move()
