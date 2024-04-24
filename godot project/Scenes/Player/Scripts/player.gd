52e188f8ed856f1ed0940712b533104f1f6ae835
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-04-24 02:15:12 -0300

    Trying to make the raycasting thing

--- godot project/Scenes/Player/Scripts/player.gd
+++ godot project/Scenes/Player/Scripts/player.gd
@@ -13,23 +13,119 @@ extends CharacterBody2D
 
 @onready var game_constants: GameConstants = load("res://Scripts/Resources/game_constants.tres")
 
+@onready var SPACE_STATE: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+
+func linear_space(start: Vector2, end: Vector2, count: int) -> Array:
+
+	var result: Array = Array()
+	var diff: Vector2 = (end - start) / (count - 1)
+
+	result.append(start)
+
+	for i in range(1, count):
+		result.append(start + diff * i)
+
+	return result
+
+func to_coord(pos: Vector2) -> Vector2i:
+	return Vector2i(floor(pos.x / game_constants.TILE_SIZE), floor(pos.y / game_constants.TILE_SIZE))
+
+func fix_pos(pos: Vector2, normal: Vector2) -> Vector2:
+	
+	if abs(normal.x) > 0.9: pos.x -= normal.x
+	if abs(normal.y) > 0.9: pos.y -= normal.y
+
+	return pos
+
+func is_valid_square(pos: Vector2, normal: Vector2) -> bool:
+	
+	var query = PhysicsRayQueryParameters2D.create(pos + 10 * normal, pos - 10 * normal)
+	
+	query.exclude = [self]
+
+	var result: Dictionary = SPACE_STATE.intersect_ray(query)
+
+	if not result:
+		return false
+	
+	if result.normal != normal:
+		return false
+
+	result.position = fix_pos(result.position, result.normal)
+
+	var coord = to_coord(result.position)
+	var tile = tile_map.get_cell_tile_data(0, coord)
+
+	return tile.get_custom_data("CanPortal")
+
+func get_squares(pos: Vector2, normal: Vector2) -> Array:
+	
+	var change: Vector2 = game_constants.PORTAL_SIZE * game_constants.TILE_SIZE * normal.rotated(PI / 2)	
+	var squares: Array = Array()
+
+	for i in linear_space(pos, pos - change, 10):
+
+		if not is_valid_square(i, normal):
+			break
+		
+		var coord = to_coord(i)
+		
+		if coord not in squares: # O(n ^ 2), but few elements, so okay
+			squares.append(coord)
+
+	for i in linear_space(pos, pos + change, 10):
+
+		if not is_valid_square(i, normal):
+			break
+		
+		var coord = to_coord(i)
+		
+		if coord not in squares: # O(n ^ 2), but few elements, so okay
+			squares.append(coord)
+
+	return squares
+
 func shoot() -> void:
 
 	var hit: Vector2 = raycast.get_collision_point()
 	var normal: Vector2 = raycast.get_collision_normal()
-	
-	if hit == null or normal == null:
-		return Vector2i(-999, -999)
+
+	if hit == Vector2.ZERO or hit == null or normal == null:
+		return
 	
 	hit -= tile_map.position
+	hit = fix_pos(hit, normal)
+
+	var squares: Array = get_squares(hit, normal)
+
+	if len(squares) < game_constants.PORTAL_SIZE:
+		return
+
+	var span1: Vector2 = Vector2.ZERO
+	var span2: Vector2 = Vector2.ZERO
+
+	print(hit, " ", ceil(hit) - hit)
+
+	print(Vector2i(floor(hit / game_constants.TILE_SIZE)), (hit / game_constants.TILE_SIZE))
+
+	for i in range(100):
+
+		if Vector2i(floor(hit / game_constants.TILE_SIZE + i * normal.rotated(PI / 2))) not in squares:
+			break
+		
+		span1 = Vector2(i, i)
+
+	for i in range(100):
+
+		if Vector2i(floor(hit / game_constants.TILE_SIZE - i * normal.rotated(PI / 2))) not in squares:
+			break
 	
-	if abs(normal.x) > .9: hit.x -= game_constants.TILE_SIZE * normal.x / 2.0
-	if abs(normal.y) > .9: hit.y -= game_constants.TILE_SIZE * normal.y / 2.0
+		span2 = Vector2(i, i)
 	
-	var x: int = floor(hit.x / game_constants.TILE_SIZE)
-	var y: int = floor(hit.y / game_constants.TILE_SIZE)
-	
-	
+	print(span1)
+	print(span2)
+
+
 func set_angle() -> void:
 	
 	var difference: Vector2 = get_global_mouse_position() - (global_position + constants.PLAYER_CENTER)
