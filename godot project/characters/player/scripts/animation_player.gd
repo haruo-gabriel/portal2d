85d305eccf69422fb321089cba9d31b2b5487103
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-05-02 20:16:09 -0300

    Fixing the crouching animation

--- godot project/characters/player/scripts/animation_player.gd
+++ godot project/characters/player/scripts/animation_player.gd
@@ -20,8 +20,13 @@ func _process(_delta: float) -> void:
 
 	var is_crouched_animation: bool = "(Crouched)" in current_animation
 
-	if player.is_crouching and (not is_crouched_animation or not player.is_on_floor()):
-		play("Idle (Crouched)")
-
 	if not player.is_crouching and is_crouched_animation:
 		play(state_machine.current_state.animation_name)
+
+	if not player.is_on_floor() or (player.is_crouching and (not is_crouched_animation or state_machine.current_state.animation_name not in current_animation)):
+
+		if state_machine.current_state.animation_name == "Walk":
+			play("Walk (Crouched)")
+		else:
+			play("Idle (Crouched)")
+
