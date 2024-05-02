4b40b14832d3d4eee150570d9c4360f9b06783c0
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-05-02 16:57:14 -0300

    Changing player constants

--- godot project/characters/player/scripts/player_constants.gd
+++ godot project/characters/player/scripts/player_constants.gd
@@ -2,10 +2,10 @@
 class_name PlayerConstants
 extends Resource
 
-const WALKING_SPEED: float = 300.0
-const HORIZONTAL_ACCELERATION: float = 20.0 # Pixels per second per frame
+const WALKING_SPEED: float = 100.0
+const HORIZONTAL_ACCELERATION: float = 10.0 # Pixels per second per frame
 
-const GROUND_DRAG: float = 15.0 # Deacceleration on ground.
+const GROUND_DRAG: float = 6.0 # Deacceleration on ground.
 
 # It's harder to change direction while on air
 const IN_AIR_MULTIPLIER: float = .3
@@ -17,10 +17,10 @@ const BHOP_MULTIPLIER: float = 1.2
 # You move slower while crouched
 const CROUCH_SPEED_MULTIPLIER: float = .5
 
-const JUMP_VELOCITY: float = -500.0
+const JUMP_VELOCITY: float = -200.0
 
-const MAX_Y_SPEED: float = 1000.0 # In absolute value
-const MAX_X_SPEED: float = 600.0 # In absolute value
+const MAX_Y_SPEED: float = 400.0 # In absolute value
+const MAX_X_SPEED: float = 300.0 # In absolute value
 
 # Frames a player can press jump before landing and still jump upon hitting ground.
 # To remove, set it to 1, NOT ZERO (the player won't be able to jump).
