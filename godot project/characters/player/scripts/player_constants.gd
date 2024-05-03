06ce70c51c4cf93e3489cc14e72b5e473fd1e110
haruo-g <takeuchigabriel@gmail.com>
2024-05-03 14:37:04 -0300

    Changing player constants and tilemap collision

--- godot project/characters/player/scripts/player_constants.gd
+++ godot project/characters/player/scripts/player_constants.gd
@@ -1,13 +1,13 @@
 
 extends Node
 
-const WALKING_SPEED: float = 100.0
-const HORIZONTAL_ACCELERATION: float = 10.0 # Pixels per second per frame
+const WALKING_SPEED: float = 200.0
+const HORIZONTAL_ACCELERATION: float = 20.0 # Pixels per second per frame
 
 const GROUND_DRAG: float = 6.0 # Deacceleration on ground.
 
 # It's harder to change direction while on air
-const IN_AIR_MULTIPLIER: float = .3
+const IN_AIR_MULTIPLIER: float = .9
 
 # Multiplies your speed upon jumping.
 # Set it to 1 to remove it.
@@ -16,7 +16,7 @@ const BHOP_MULTIPLIER: float = 1.2
 # You move slower while crouched
 const CROUCH_SPEED_MULTIPLIER: float = .5
 
-const JUMP_VELOCITY: float = -200.0
+const JUMP_VELOCITY: float = -250.0
 
 const MAX_Y_SPEED: float = 400.0 # In absolute value
 const MAX_X_SPEED: float = 300.0 # In absolute value
