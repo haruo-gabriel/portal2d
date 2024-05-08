97e3ba0609c65b598fa323d9c7f2730ed903ba93
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-05-08 09:50:41 -0300

    Making portals work with a single scene

--- godot project/portals/portals_constants.gd
+++ godot project/portals/portals_constants.gd
@@ -4,3 +4,8 @@ enum PortalType {
 	PORTAL_TYPE_ORANGE,
 	PORTAL_TYPE_BLUE
 }
+
+const PORTAL_COLORS: Dictionary = {
+	PortalType.PORTAL_TYPE_ORANGE : Color.ORANGE,
+	PortalType.PORTAL_TYPE_BLUE : Color.AQUA
+}
