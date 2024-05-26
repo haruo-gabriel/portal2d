4f9481c800b88151c54bc71cf58967acd0977bc9
haruo <61366105+haruo-g@users.noreply.github.com>
2024-05-26 17:31:25 -0300

    Update player_constants.gd
    Changing GROUND_DRAG and MAX_Y_SPEED

--- godot project/characters/player/scripts/player_constants.gd
+++ godot project/characters/player/scripts/player_constants.gd
@@ -4,7 +4,8 @@ extends Node
 const WALKING_SPEED: float = 200.0
 const HORIZONTAL_ACCELERATION: float = 20.0 # Pixels per second per frame
 
-const GROUND_DRAG: float = 6.0 # Deacceleration on ground.
+# const GROUND_DRAG: float = 6.0 # Deacceleration on ground
+const GROUND_DRAG: float = 48.0 # Deacceleration on ground.
 
 # It's harder to change direction while on air
 const IN_AIR_MULTIPLIER: float = .8
@@ -20,7 +21,8 @@ const CROUCH_SPEED_MULTIPLIER: float = .5
 
 const JUMP_VELOCITY: float = -300.0
 
-const MAX_Y_SPEED: float = 400.0 # In absolute value
+# const MAX_Y_SPEED: float = 400.0 # In absolute value
+const MAX_Y_SPEED: float = 1200.0 # In absolute value
 const MAX_X_SPEED: float = 600.0 # In absolute value
 
 # Frames a player can press jump before landing and still jump upon hitting ground.
