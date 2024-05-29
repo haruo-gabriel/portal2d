0bd80e81233aa5db2e5a2527c49a7dc864c7b21c
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-05-29 09:41:18 -0300

    Adding player detection areas
    the group name is `MovingEnemyTarget`

--- godot project/characters/moving_enemy/scripts/states/state_machine.gd
+++ godot project/characters/moving_enemy/scripts/states/state_machine.gd
@@ -6,3 +6,17 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	super(delta)
+
+func _on_player_sight_area_body_entered(body: Node2D) -> void:
+
+	if not body.is_in_group("MovingEnemyTarget"):
+		return
+		
+	current_state.transitioned.emit(current_state, "chasing")
+
+func _on_player_attack_area_body_entered(body: Node2D) -> void:
+
+	if not body.is_in_group("MovingEnemyTarget"):
+		return
+
+	current_state.transitioned.emit(current_state, "attack")
