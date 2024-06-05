5a95ab6483087bf8209f79f463883783226d77ce
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-06-05 11:26:01 -0300

    Adding a background

---
+++ godot project/levels/level_1/ParallaxBackground.gd
@@ -0,0 +1,15 @@
+
+extends ParallaxBackground
+
+@export var player: Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
